7e488f22a0b3a146eb93ee54abd08eda50d9e089
kanishkk19 <kannu1904@gmail.com>
2023-01-27 16:10:37 +0530

    changes

--- js/main.js
+++ js/main.js
@@ -1,56 +1,58 @@
-const task_input = document.querySelector('input');
-const add_btn = document.querySelector('.add-task-button');
-const todos_list = document.querySelector('.todos-list');
-const alert_message = document.querySelector('.alert-message');
-const delete_all_btn = document.querySelector('.delete-all-btn');
+const task_input = document.querySelector("input");
+const add_btn = document.querySelector(".add-task-button");
+const todos_list = document.querySelector(".todos-list");
+const alert_message = document.querySelector(".alert-message");
+const delete_all_btn = document.querySelector(".delete-all-btn");
 
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
-let todos = JSON.parse(localStorage.getItem('todos')) || [];
-
-window.addEventListener('DOMContentLoaded', showAllTodos);
+window.addEventListener("DOMContentLoaded", showAllTodos);
 
 //get random unique id
 function getRandomId() {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  );
 }
 
 function addToDo(task_input) {
-    let task = {
-        id: getRandomId(),
-        task: task_input.value,
-        completed: false
-    }
-    todos.push(task);
+  let task = {
+    id: getRandomId(),
+    task: task_input.value,
+    completed: false,
+  };
+  todos.push(task);
 }
 
-task_input.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13 && task_input.value.length > 0) {
-        addToDo(task_input);
-        saveToLocalStorage();
-        task_input.value = '';
-        showAllTodos();
-    }
+task_input.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13 && task_input.value.length > 0) {
+    addToDo(task_input);
+    saveToLocalStorage();
+    task_input.value = "";
+    showAllTodos();
+  }
 });
 
-add_btn.addEventListener('click', () => {
-    if (task_input.value === '') {
-        showAlertMessage('Please enter a task', 'error');
-    } else {
-        addToDo(task_input);
-        saveToLocalStorage();
-        showAllTodos();
-        task_input.value = '';
-        showAlertMessage('Task added successfully', 'success');
-    }
+add_btn.addEventListener("click", () => {
+  if (task_input.value === "") {
+    showAlertMessage("Please enter a task", "error");
+  } else {
+    addToDo(task_input);
+    saveToLocalStorage();
+    showAllTodos();
+    task_input.value = "";
+    showAlertMessage("Task added successfully", "success");
+  }
 });
 
-delete_all_btn.addEventListener('click', clearAllTodos);
+delete_all_btn.addEventListener("click", clearAllTodos);
 
 //show all todos
 function showAllTodos() {
-    todos_list.innerHTML = '';
-    todos.forEach((todo) => {
-        todos_list.innerHTML += `
+  todos_list.innerHTML = "";
+  todos.forEach((todo) => {
+    todos_list.innerHTML += `
             <li class="todo-item" data-id="${todo.id}">
                 <p class="task-body">
                     ${todo.task}
@@ -65,17 +67,17 @@ function showAllTodos() {
                 </div>
             </li>
         `;
-    });
+  });
 }
 
 //save todos to local storage
 function saveToLocalStorage() {
-    localStorage.setItem('todos', JSON.stringify(todos));
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 //show alert message
 function showAlertMessage(message, type) {
-    let alert_box = `
+  let alert_box = `
         <div class="alert alert-${type} shadow-lg mb-5 w-full">
             <div>
                 <span>
@@ -83,45 +85,45 @@ function showAlertMessage(message, type) {
                 </span>
             </div>
         </div>
-    `
-    alert_message.innerHTML = alert_box;
-    alert_message.classList.remove('hide');
-    alert_message.classList.add('show');
-    setTimeout(() => {
-        alert_message.classList.remove('show');
-        alert_message.classList.add('hide');
-    }, 3000);
+    `;
+  alert_message.innerHTML = alert_box;
+  alert_message.classList.remove("hide");
+  alert_message.classList.add("show");
+  setTimeout(() => {
+    alert_message.classList.remove("show");
+    alert_message.classList.add("hide");
+  }, 3000);
 }
 
 //delete todo
 function deleteTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-    saveToLocalStorage();
-    showAlertMessage('Todo deleted successfully', 'success');
-    showAllTodos();
+  todos = todos.filter((todo) => todo.id !== id);
+  saveToLocalStorage();
+  showAlertMessage("Todo deleted successfully", "success");
+  showAllTodos();
 }
 
 //edit todo
 function editTodo(id) {
-    let todo = todos.find(todo => todo.id === id);
-    task_input.value = todo.task;
-    todos = todos.filter(todo => todo.id !== id);
-    add_btn.innerHTML = "<i class='bx bx-check bx-sm'></i>";
-    saveToLocalStorage();
-    add_btn.addEventListener('click', () => {
-        add_btn.innerHTML = "<i class='bx bx-plus bx-sm'></i>"; 
-        showAlertMessage('Todo updated successfully', 'success');
-    });
+  let todo = todos.find((todo) => todo.id === id);
+  task_input.value = todo.task;
+  todos = todos.filter((todo) => todo.id !== id);
+  add_btn.innerHTML = "<i class='bx bx-check bx-sm'></i>";
+  saveToLocalStorage();
+  add_btn.addEventListener("click", () => {
+    add_btn.innerHTML = "<i class='bx bx-plus bx-sm'></i>";
+    showAlertMessage("Todo updated successfully", "success");
+  });
 }
 
 //clear all todos
 function clearAllTodos() {
-    if(todos.length > 0) {
-        todos = [];
-        saveToLocalStorage();
-        showAlertMessage('All todos cleared successfully', 'success');
-        showAllTodos();
-    }else{
-        showAlertMessage('No todos to clear', 'error');
-    }
+  if (todos.length > 0) {
+    todos = [];
+    saveToLocalStorage();
+    showAlertMessage("All todos cleared successfully", "success");
+    showAllTodos();
+  } else {
+    showAlertMessage("No todos to clear", "error");
+  }
 }
